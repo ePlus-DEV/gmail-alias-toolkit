d557e6b7175643c44f9d6f132a2921429b0e154b
David Nguyen <h250694@gmail.com>
2025-12-30 09:17:32 +0700

    feat: update icons and add auto-icons module to enhance visual representation

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "wxt";
+import { EventEmitter } from "events";
+
+// Fix EventEmitter maxListeners warning
+EventEmitter.defaultMaxListeners = 15;
 
 export default defineConfig({
-  modules: ["@wxt-dev/module-react"],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
   manifest: {
     name: "Gmail Alias Toolkit",
     description:
@@ -13,22 +17,8 @@ export default defineConfig({
         id: "{71243e5a-8ec2-41a5-8ef5-f2861ebd8fed}",
       },
     },
-    icons: {
-      16: "/icon/16.png",
-      32: "/icon/32.png",
-      48: "/icon/48.png",
-      96: "/icon/96.png",
-      128: "/icon/128.png",
-    },
-    action: {
-      default_icon: {
-        16: "/icon/16.png",
-        32: "/icon/32.png",
-        48: "/icon/48.png",
-        96: "/icon/96.png",
-        128: "/icon/128.png",
-      },
-      default_title: "Gmail Alias Toolkit",
-    },
+  },
+  autoIcons: {
+    developmentIndicator: "overlay",
   },
 });
