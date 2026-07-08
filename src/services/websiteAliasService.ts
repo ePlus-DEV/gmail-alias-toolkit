66e97f8a4fb23eb153906ec8e822a6c032578090
David Nguyen <h250694@gmail.com>
2026-07-08 14:23:13 +0700

    fix: correct import path in websiteAliasService.ts

--- src/services/websiteAliasService.ts
+++ src/services/websiteAliasService.ts
@@ -1,5 +1,5 @@
 import { normalizeHostname } from "../utils/hostnameNormalizer";
-import { getAccountStorageKey } from "../components/popup/utils";
+import { getAccountStorageKey } from "../../entrypoints/popup/utils";
 
 interface WebsiteAliasEntry {
   alias: string;
